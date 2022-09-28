886542f66c22afbf12fef0be3f0f754960f66523
Bruno Risso <brunorisso58@gmail.com>
2022-09-28 00:57:12 -0300

    Fazendo o formulário funcionar

--- src/components/FormCadastro.vue
+++ src/components/FormCadastro.vue
@@ -1,32 +1,32 @@
 <template>
+    <span v-show="this.msg" class="msg">{{ this.msg }}</span>
     <form id="create-patient-form" @submit="createPatient">
         <div class="field">
             <label for="name" class="label-title">Nome</label><br>
-            <input type="text" class="input input-text" id="name" placeholder="Nome..." v-model="name">
+            <input type="text" v-model="name" class="input input-text" id="name" placeholder="Nome..." required>
         </div>
         <div class="field">
             <label for="gender" class="label-title">Gênero</label><br>
-            <input type="radio" class="input-radio" name="gender" id="male">
+            <input type="radio" v-model="gender" value="M" class="input-radio" name="gender" id="male">
             <label for="male">Masculino</label>
-            <input type="radio" class="input-radio" name="gender" id="female">
-            <label for="female">Feminino</label>
+            <input type="radio" v-model="gender" value="F" class="input-radio" name="gender" id="female" required>
+            <label for="female" required>Feminino</label>
         </div>
         <div class="field">
             <label for="birthday" class="label-title">Data de Nascimento</label><br>
-            <input type="date" class="input" name="birthday" id="birthday">
+            <input type="date" v-model="birthday" class="input" name="birthday" id="birthday" required>
         </div>
         <div class="field">
             <label for="address" class="label-title">Endereço</label><br>
-            Rua: <br><input type="text" placeholder="Rua..." name="street" class="input input-text" id="street"><br>
-            Número: <br><input type="text" placeholder="Número..." name="number" class="input input-text" id="number"><br>
-            Bairro: <br><input type="text" placeholder="Bairro..." name="neighborhood" class="input input-text" id="neighborhood"><br>
-            Cidade: <br><input type="text" placeholder="Cidade..." name="city" class="input input-text" id="city"><br>
-            Estado: <br><input type="text" placeholder="Estado..." name="state" class="input input-text" id="state"><br>
+            Rua: <br><input type="text" v-model="street" placeholder="Rua..." name="street" class="input input-text" id="street" required><br>
+            Número: <br><input type="text" v-model="number" placeholder="Número..." name="number" class="input input-text" id="number" required><br>
+            Bairro: <br><input type="text" v-model="neighborhood" placeholder="Bairro..." name="neighborhood" class="input input-text" id="neighborhood" required><br>
+            Cidade: <br><input type="text" v-model="city" placeholder="Cidade..." name="city" class="input input-text" id="city" required><br>
+            Estado: <br><input type="text" v-model="state" placeholder="Estado..." name="state" class="input input-text" id="state" required><br>
         </div>
         <div class="field">
             <label for="plan" class="label-title">Plano</label><br>
-            <select class="input" name="plans" id="plans">
-                <option value="">Selecione o plano</option>
+            <select v-model="plan" class="input" name="plans" id="plans" required>
                 <option value="1">Básico</option>
                 <option value="2">Intermediário</option>
                 <option value="3">Avançado</option>
@@ -41,13 +41,60 @@ export default {
     name: "FormCadastro",
     data() {
         return {
-            name: null
+            name: null,
+            gender: null,
+            birthday: null,
+            street: null,
+            number: null,
+            neighborhood: null,
+            city: null,
+            state: null,
+            plan: null,
+            msg: null
         }
     },
     methods: {
-        createPatient(e) {
+        async createPatient(e) {
             e.preventDefault();
-            console.log("Enviou!");
+            const data = {
+                name: this.name,
+                birthday: this.birthday,
+                gender: this.gender,
+                adress: {
+                    street: this.street,
+                    number: this.number,
+                    neighborhood: this.neighborhood,
+                    city: this.city,
+                    state: this.state
+                },
+                planId: this.plan
+            }
+            const dataJson = JSON.stringify(data);
+
+            const req = await fetch("http://localhost:3000/patients", {
+                method: "POST",
+                headers: { "Content-Type": "application/json" },
+                body: dataJson
+            });
+            const res = await req.json();
+            console.log(res);
+
+            this.msg = "Paciente adicionado com sucesso!";
+            
+            setTimeout(() => {
+                this.msg = null;
+            }, 4000)
+
+            this.name = null,
+            this.gender = null,
+            this.birthday = null,
+            this.street = null,
+            this.number = null,
+            this.neighborhood = null,
+            this.city = null,
+            this.state = null,
+            this.plan = null
+
         }
     }
 }
@@ -86,7 +133,6 @@ export default {
         padding: 10px
     }
 
-
     .input-submit {
         border: none;
         width: 15%;
@@ -98,5 +144,14 @@ export default {
 
     .input-submit:hover {
         background-color: #5cb9e4cb;
+    }
+
+    .msg {
+        background-color: #2ECC71;
+        border-radius: 15px;
+        padding: 10px;
+        position: fixed;
+        top: 20px;
+        right: 20px;
     }
 </style>
