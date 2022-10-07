268a2982b344d29cd524445afad526f312d2f8a3
Bruno Risso <brunorisso58@gmail.com>
2022-10-07 18:28:42 -0300

    Ajustando server.js

--- server.js
+++ server.js
@@ -1,14 +1,7 @@
-const jsonServer = require('json-server')
-const server = jsonServer.create()
-const router = jsonServer.router('db/db.json')
-
-const middlewares = jsonServer.defaults()
-
-server.use(middlewares)
-server.use(router)
-
-const port = process.env.PORT || 3000
-
-server.listen(port, () => {
-    console.log(`JSON Server is running on port ${port}`)
-})
+const express = require('express');
+const serveStatic = require("serve-static")
+const path = require('path');
+app = express();
+app.use(serveStatic(path.join(__dirname, 'dist')));
+const port = process.env.PORT || 3000;
+app.listen(port);
