54973d527c06907d3ddb4fd452a929532389a7c0
Bruno Risso <brunorisso58@gmail.com>
2022-10-07 23:28:09 -0300

    Ajustando server.js

--- server.js
+++ server.js
@@ -5,8 +5,6 @@ app = express();
 app.use(serveStatic(path.join(__dirname, 'dist')));
 const port = process.env.PORT || 3000;
 app.listen(port);
-
-var path = require('path')
 app.route('/*')
     .get(function(req, res) {
           res.sendFile(path.join(__dirname + '/dist/index.html'));
